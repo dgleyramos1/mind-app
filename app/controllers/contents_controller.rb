e917e9046fb3c70db914976f9451ecf39563cc5a
dgleyramos1 <dgleysilva@gmail.com>
2021-06-16 12:50:27 -0300

    Adiciona tela de edição de conteúdo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,4 +7,22 @@ class ContentsController < ApplicationController
     def new
         @Content =  Content.new
     end
+    def create
+        @content = current_user.contents.buidl(content_params)
+
+        if @content.save
+            redirect_to contents_path, notice: 'Content successfully created'
+        else
+            render :new
+        end
+    end
+    def edit
+        @content = Content.find(params[:id])
+    end
+
+
+    private
+
+    def content_params
+        params.requere(:content).permit(:title, :description)
 end
